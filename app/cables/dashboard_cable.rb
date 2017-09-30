a46e641d35dc3449fc2df2204b08da6facd40499
Daniel Westendorf <daniel@prowestech.com>
2017-09-30 14:37:13 -0600

    Add dashboard cable

---
+++ app/cables/dashboard_cable.rb
@@ -0,0 +1,14 @@
+class DashboardCable < BaseCable
+  def fibonacci
+    n = SecureRandom.rand(20..40)
+    calculated = calculate_fibonacci(n)
+    stream partial: "dashboard/fibonacci", locals: { calculated: calculated, n: n }
+  end
+
+  private
+
+  def calculate_fibonacci(n)
+    return n if n <= 1
+    calculate_fibonacci( n - 1 ) + calculate_fibonacci( n - 2 )
+  end
+end
